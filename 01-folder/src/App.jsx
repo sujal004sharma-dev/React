503d2a57c238926faf61192d699d20054c1cbfc9
Sujal <sujal004sharma@gmail.com>
2026-03-21 17:38:51 +0530

    01-Folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    //these empty brackets are called fragements.
+    <>
+      
+        <div id="dad">
+          <h1>APP</h1>
+          <h3>app</h3>
+        </div>
+        <div id="chacha"></div>
+      
+    </>
+
+  )
+}
+
+export default App
